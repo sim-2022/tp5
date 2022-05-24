ba8825c8536e60e8dc86f1b3e719639bd8080b90
Juan Cruz <juancruzfaq@gmail.com>
2022-05-24 15:21:10 -0300

    refactor: borrar métodos no utilizados

--- tp5/Modelos/Dominio/Sector.cs
+++ tp5/Modelos/Dominio/Sector.cs
@@ -6,24 +6,12 @@ namespace tp5.Modelos.Dominio
     public struct Sector
     {
         public int Id { get; private set; }
-        public TipoAuto? TipoAuto { get; private set; }
         public int Salida { get; private set; }
-        public static Sector CrearSectorLibre(int id) => new Sector() { Id = id };
-        public EstadoSector EstadoSector => TipoAuto is null ? EstadoSector.Libre : EstadoSector.Ocupado;
         public string Estado => EstadoSector.ObtenerDescripcion();
+        public TipoAuto? TipoAuto { get; private set; }
+        public EstadoSector EstadoSector => TipoAuto is null ? EstadoSector.Libre : EstadoSector.Ocupado;
         
-        public void Estacionar(TipoAuto tipoAuto, int tiempoRelojSalida)
-        {
-            TipoAuto = tipoAuto;
-            Salida = tiempoRelojSalida;
-        }
-        
-        public void Desocupar()
-        {
-            TipoAuto = null;
-            Salida = -1;
-        }
-
+        public static Sector CrearSectorLibre(int id) => new Sector() { Id = id };
         public static Sector CrearSectorOcupado(int sectorLibreId, TipoAuto auto, int tiempoRelojSalida)
         {
             return new Sector()
